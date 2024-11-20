03410fff2b9eac86d315d51eb759585753ce9e14
Elaine Fang <efang@lcs.on.ca>
2024-11-20 10:11:38 -0500

    Initial Commit

---
+++ Seasons/SeasonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeasonsApp.swift
+//  Seasons
+//
+//  Created by Fang Elaine on 2024/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SeasonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
